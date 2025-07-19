742ed6ab43903d61a8af1afbce0f4a9f17f5b8b9
517milky <517milky@gmail.com>
2025-07-19 18:00:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
 
-url = "여기에 네가 넣은 유튜브 링크"  # 예: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+url = "https://www.youtube.com/shorts/eycVVvODZhU"  # 예: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 yt = YouTube(url)
 print(yt.title)
